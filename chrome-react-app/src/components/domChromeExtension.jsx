699ed4731676fef7bf1f417b2e78f9f38a0a8959
sam453 <sam@obsessvr.com>
2023-03-06 17:55:17 -0500

    prettier and other updates

--- chrome-react-app/src/components/domChromeExtension.jsx
+++ chrome-react-app/src/components/domChromeExtension.jsx
@@ -1,32 +1,37 @@
-import React, { useEffect } from "react";
-import { waitForElm } from "../utils/domFunctions.js";
-import { getAllCards } from "../utils/apiUtils.js";
-import { getKingdom } from "../utils/dominionFunctions.js";
+import React, { useEffect } from 'react';
+import { waitForElm, watchElm } from '../utils/domFunctions.js';
+import { getAllCards } from '../utils/apiUtils.js';
+import { getKingdom } from '../utils/dominionFunctions.js';
 
 const DomChromeExtension = () => {
-  const run = () => {
-    getKingdom().then((kingdom) => {
-      console.log("====>", kingdom);
-    });
-  };
+    const run = () => {
+        getKingdom().then((kingdom) => {
+            console.log('====>', kingdom);
+            watchElm('.log-scroll-container', (w) => {
+                if (w.addedNodes[0].childNodes.length > 0) {
+                    console.log('====>', w.addedNodes[0]);
+                }
+            });
+        });
+    };
 
-  const init = () => {
-    getAllCards().then((cards) => {
-      console.log("====>", cards);
-    });
-    waitForElm(".game-area").then(() => {
-      run();
-    });
-  };
+    const init = () => {
+        getAllCards().then((cards) => {
+            console.log('====>', cards);
+        });
+        waitForElm('.game-area').then(() => {
+            run();
+        });
+    };
 
-  useEffect(() => {
-    init();
-  }, []);
+    useEffect(() => {
+        init();
+    }, []);
 
-  return (
-    <div>
-      <h1>Dom Chrome Extension</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Dom Chrome Extension</h1>
+        </div>
+    );
 };
 export default DomChromeExtension;
